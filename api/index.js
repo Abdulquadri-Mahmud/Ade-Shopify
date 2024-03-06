a995d8dd06b786d2b8aa456b7bde1d4d185795ca
Abdulquadri-Mahmud <114073342+Abdulquadri-Mahmud@users.noreply.github.com>
2024-03-06 15:24:29 +0100

    server and database created successfully

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 import express from "express";
-// import mongoose from ''
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
 const app = express();
 
-// app.use(express.json());
-
-
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.use(express.json());
+// console.log(process.env.database);
+mongoose.connect(process.env.db).then(() => {
+    console.log('database connected!');
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+}).catch((error)=> {
+    console.log(error,'Error while connecting to database!');
 });
